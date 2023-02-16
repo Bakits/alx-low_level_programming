99a1c92c864d3a1030d47d004143bd2e76141ad7
Bakits <josephinebakita34@gmail.com>
2023-02-16 02:00:29 -0800

    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

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *  main -  A program that prints a line with puts function
+ *  main -  A program that prints a line using the print function
  *  Return: 0 (Sucess)
  */
 int main(void)
