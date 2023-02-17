43a49103b3663fd4fbfc5d569b63451b07fe8b9c
Bakits <josephinebakita34@gmail.com>
2023-02-17 07:33:20 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,12 +10,12 @@
 */
 int main(void)
 {
-int d, p, q
+int d, p, q;
 for (d = '0'; d < '9'; d++)
 {
-for (p = d + 1; p < = '9'; p++)
+for (p = d + 1; p <= '9'; p++)
 {
-for (q = p + 1; q < = '9'; q++)
+for (q = p + 1; q <= '9'; q++)
 {
 if ((p != d) != q)
 {
@@ -24,10 +24,12 @@ putchar(p);
 putchar(q);
 if (d == '7' && p == '8')
 continue;
-purchar(',');
+putchar(',');
 putchar(' ');
+}
 }
 }
 }
 putchar('\n');
 return (0);
+}
