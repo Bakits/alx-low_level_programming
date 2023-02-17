1a935e4737ae13b4d6dc8220d5a7a4d559a0dbf5
Bakits <josephinebakita34@gmail.com>
2023-02-17 03:48:34 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,14 @@
 int main(void)
 {
 	int num;
-
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
+for (num = 0; num <= 9; num++)
+{
+                putchar((num % 10) + '0');
 		if (num == 9)
-			continue;
-		
+		continue;
 		putchar(',');
 		putchar(' ');
-	}
-
-	putchar('\n');
-
+}
+               putchar('\n');
 return (0);
 }
