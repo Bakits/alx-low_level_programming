176864001d23fc97d0481b9da5b453403763e95d
Bakits <josephinebakita34@gmail.com>
2023-03-18 02:20:59 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,16 +15,13 @@ int wildcmp(char *s1, char *s2);
 int strlen_no_wilds(char *str)
 {
 int len = 0, index = 0;
-
 if (*(str + index))
 {
 if (*str != '*')
 len++;
-
 index++;
 len += strlen_no_wilds(str + index);
 }
-
 return (len);
 }
 
@@ -41,7 +38,6 @@ if (**wildstr == '*')
 iterate_wild(wildstr);
 }
 }
-
 /**
  * postfix_match - Checks if a string str matches the postfix of
  *                 another string potentially containing wildcards.
@@ -56,17 +52,13 @@ char *postfix_match(char *str, char *postfix)
 {
 int str_len = strlen_no_wilds(str) - 1;
 int postfix_len = strlen_no_wilds(postfix) - 1;
-
 if (*postfix == '*')
 iterate_wild(&postfix);
-
 if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
 {
 postfix++;
 return (postfix_match(str, postfix));
 }
-
-	
 return (postfix);
 }
 
@@ -85,12 +77,9 @@ if (*s2 == '*')
 iterate_wild(&s2);
 s2 = postfix_match(s1, s2);
 }
-
 if (*s2 == '\0')
 return (1);
-
 if (*s1 != *s2)
 return (0);
-
 return (wildcmp(++s1, ++s2));
 }
