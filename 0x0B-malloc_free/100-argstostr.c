10d821e844052cff613b285ab9e65e7622f293d2
Bakits <josephinebakita34@gmail.com>
2023-03-18 06:07:14 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,86 +1,46 @@
 #include "main.h"
 #include <stdlib.h>
-
-int word_len(char *str);
-int count_words(char *str);
-char **strtow(char *str);
+#include <stdio.h>
 
 /**
- * word_len - Locates the index marking the end of the
- *            first word contained within a string.
- * @str: The string to be searched.
- *
- * Return: The index marking the end of the initial word pointed to by str.
- */
-int word_len(char *str)
+  * argstostr - convert the params passed to the program to string
+  * @ac: the argument count
+  * @av: the argument vector
+  *
+  * Return: ...
+  */
+char *argstostr(int ac, char **av)
 {
-int index = 0, len = 0;
-while (*(str + index) && *(str + index) != ' ')
-{
-len++;
-index++;
-}
-return (len);
-}
-
-/**
- * count_words - Counts the number of words contained within a string.
- * @str: The string to be searched.
- *
- * Return: The number of words contained within str.
- */
-int count_words(char *str)
-{
-int index = 0, words = 0, len = 0;
-for (index = 0; *(str + index); index++)
-len++;
-for (index = 0; index < len; index++)
-{
-if (*(str + index) != ' ')
-{
-words++;
-index += word_len(str + index);
-}
-}
-return (words);
-}
-
-/**
- * strtow - Splits a string into words.
- * @str: The string to be split.
- *
- * Return: If str = NULL, str = "", or the function fails - NULL.
- *         Otherwise - a pointer to an array of strings (words).
- */
-char **strtow(char *str)
-{
-char **strings;
-int index = 0, words, w, letters, l;
-if (str == NULL || str[0] == '\0')
+int ch = 0, i = 0, j = 0, k = 0;
+char *s;
+if (ac == 0 || av == NULL)
 return (NULL);
-words = count_words(str);
-if (words == 0)
-return (NULL);
-strings = malloc(sizeof(char *) * (words + 1));
-if (strings == NULL)
-return (NULL);
-for (w = 0; w < words; w++)
+while (i < ac)
 {
-while (str[index] == ' ')
-index++;
-letters = word_len(str + index);
-strings[w] = malloc(sizeof(char) * (letters + 1));
-if (strings[w] == NULL)
+while (av[i][j])
 {
-for (; w >= 0; w--)
-free(strings[w]);
-free(strings);
-return (NULL);
+ch++;
+j++;
 }
-for (l = 0; l < letters; l++)
-strings[w][l] = str[index++];
-strings[w][l] = '\0';
+j = 0;
+i++;
 }
-strings[w] = NULL;
-return (strings);
+s = malloc((sizeof(char) * ch) + ac + 1);
+i = 0;
+while (av[i])
+{
+while (av[i][j])
+{
+s[k] = av[i][j];
+k++;
+j++;
+}
+s[k] = '\n';
+j = 0;
+k++;
+i++;
+}
+k++;
+s[k] = '\0';
+return (s);
 }
