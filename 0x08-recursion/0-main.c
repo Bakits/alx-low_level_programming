7103539efbc58f1dc9ef39d97274a8ae8281af97
Bakits <josephinebakita34@gmail.com>
2023-03-18 00:21:26 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "../main.h"
+
+/**
+ * main - check the code for ALX students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+_puts_recursion("Betty ALX");
+return (0);
+}
---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,12 @@
+#include "../main.h"
+
+/**
+ * main - check the code for ALX students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_rev_recursion("\nColton Walker");
+return (0);
+}
